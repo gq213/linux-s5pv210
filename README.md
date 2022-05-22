056515aa5c2849ce7efee50252beeb017ffed62b
gq213 <gaoqiang1211@gmail.com>
2022-05-22 13:01:52 +0800

    pack_kernel.sh support argument.

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ make menuconfig
 
 make zImage -j4
 make 210-sate210.dtb
-cat arch/arm/boot/zImage arch/arm/boot/dts/210-sate210.dtb > zImagedtb
+./pack_kernel.sh 210-sate210.dtb
 make 210-tq210v4.dtb
-cat arch/arm/boot/zImage arch/arm/boot/dts/210-tq210v4.dtb > zImagedtb
+./pack_kernel.sh 210-tq210v4.dtb
 
 make modules -j4
 make modules_install INSTALL_MOD_PATH=/mnt/fr/modules
--- pack_kernel.sh
+++ pack_kernel.sh
@@ -7,8 +7,12 @@ TMP_IMG=./tmp_img
 
 rm -rf ${SRC_KERNEL}
 
-#cat arch/arm/boot/zImage arch/arm/boot/dts/210-sate210.dtb > ${SRC_KERNEL}
-cat arch/arm/boot/zImage arch/arm/boot/dts/210-tq210v4.dtb > ${SRC_KERNEL}
+if [ ! -f arch/arm/boot/dts/$1 ]; then
+	echo "not found arch/arm/boot/dts/$1"
+	exit -1
+fi
+
+cat arch/arm/boot/zImage arch/arm/boot/dts/$1 > ${SRC_KERNEL}
 
 rm -rf ${DST_KERNEL}
 
